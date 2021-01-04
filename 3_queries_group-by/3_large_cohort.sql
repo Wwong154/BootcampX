ed19ab3cfaffb4312321eb91bb5124f58bf25c12
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-01-04 19:44:34 +0000

    added assignment for 3_queries_group-by, total of 6

---
+++ 3_queries_group-by/3_large_cohort.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name AS cohort_name, count(students.name) AS student_count
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.name) >= 18
+ORDER BY student_count
