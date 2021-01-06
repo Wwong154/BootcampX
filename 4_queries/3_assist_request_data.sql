84ebe8347ec4a9eabd813a7b4b6e95195e4f5888
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-01-06 21:00:33 +0000

    set up the JS project

--- 4_queries/3_assist_request_data.sql
+++ 4_queries/3_assist_request_data.sql
@@ -1,6 +1,6 @@
-SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (completed_at - started_at) AS duration
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
 FROM assistance_requests
-JOIN teachers ON teacher_id = teachers.id
+JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
-JOIN assignments ON assignment_id = assignments.id
-ORDER BY duration
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
