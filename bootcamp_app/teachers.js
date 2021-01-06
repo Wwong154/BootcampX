c50e05b05ab4b26895a7699e8b336a03a7151255
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-01-06 22:22:55 +0000

    parameterized query completed

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,18 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-const filter = process.argv.slice(2);
-
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${filter}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher
-`)
+`
+const value = [`%${process.argv.slice(2)}%`];
+
+pool.query(queryString, value)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
